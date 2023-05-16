c59d088407c5e6d5300a077240b9860472a8050f
ErinTomorri <97967950+ErinTomorri@users.noreply.github.com>
2023-05-16 03:33:54 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 package pack4;
+
 import java.text.NumberFormat;
 import java.util.*;
 import java.io.*; // Import the File class
@@ -15,18 +16,26 @@ Description: This program will display the code and allow the user to input thei
 Names, Distance from dummy, angle, velocity, gravity,height
 1.Erin,20,30,25,9.8,10
  */
+
 public class Main {
-    public static void write(Projectile projectile[], int numPeople, int count) { //this is the wrting function this will write over the txt file with the new text
+    public static void write(Projectile projectile[], int numPeople, int count) { // this is the wrting function this
+                                                                                  // will write over the txt file with
+                                                                                  // the new text
         NumberFormat decimal = NumberFormat.getNumberInstance();
         try {
             FileWriter writer = new FileWriter("text.txt"); // finds the text file
-            String temp = "Names, Distance from dummy, Angle, Velocity, Gravity, Height \n"; // allocates the temp with a the title and coloums of the menus
-            for (int i = 0; i < numPeople + count+1; i++) {
-                // this gets all the data such as names, dummy distance, angle, velocity and etc and stores it to the end of temp
-                temp = temp + i; 
-                temp = temp + "." + projectile[i].getNames() + "," + projectile[i].getDummy() + "," + decimal.format(projectile[i].getAngle()*180/Math.PI) + "," + projectile[i].getVelocity() + "," + projectile[i].getAcceleration() + "," + projectile[i].getHeight() + "\n";
+            String temp = "Names, Distance from dummy, Angle, Velocity, Gravity, Height \n"; // allocates the temp with
+                                                                                             // a the title and coloums
+                                                                                             // of the menus
+            for (int i = 0; i < numPeople + count; i++) {
+                // this gets all the data such as names, dummy distance, angle, velocity and etc
+                // and stores it to the end of temp
+                temp = temp + i;
+                temp = temp + "." + projectile[i].getNames() + "," + projectile[i].getDummy() + ","
+                        + decimal.format(projectile[i].getAngle() * 180 / Math.PI) + "," + projectile[i].getVelocity()
+                        + "," + projectile[i].getAcceleration() + "," + projectile[i].getHeight() + "\n";
             }
-            writer.write(temp); //writes the txt file
+            writer.write(temp); // writes the txt file
             writer.close();
         } catch (IOException e) { // a basic exception to check if everything works
             e.printStackTrace();
@@ -34,103 +43,102 @@ public class Main {
     }
 
     public static int count() { // this opens the file and checks how many projectiles were in the past
-        String filePath = "text.txt"; //opens the text file
+        String filePath = "text.txt"; // opens the text file
         File file = new File(filePath);
-        int count = -1; //starts the count of the lines at -1 because the first line is the "title" 
+        int count = -1; // starts the count of the lines at -1 because the first line is the "title"
 
-        if (file.exists()) { // checks if the file exists 
+        if (file.exists()) { // checks if the file exists
             System.out.println("File exists.");
         } else {
             System.out.println("File does not exist.");
         }
         try {
             File myObj = new File("text.txt");
-            Scanner myReader = new Scanner(myObj); //opens the file and scans it
+            Scanner myReader = new Scanner(myObj); // opens the file and scans it
             while (myReader.hasNextLine()) { // checks if it has a next line
-                myReader.nextLine(); // checks the next line to see if there is something 
+                myReader.nextLine(); // checks the next line to see if there is something
                 count++; // counts the projectiles
             }
             myReader.close();
             return count; // returns the number of projectiles
 
-        } catch (FileNotFoundException e) { //basic exection error
+        } catch (FileNotFoundException e) { // basic exection error
             System.out.println("An error occurred.");
             e.printStackTrace(); // trace the error
         }
         return count;
     }
-    public static void read(int count, Projectile projectile[]) { // reads the file and allocates the projectiles arrays filled with the text file
-        String prev;
-        NumberFormat decimal = NumberFormat.getNumberInstance();
-        try {
-            File myObj = new File("text.txt"); //opens the text file
 
+    public static void read(int count, Projectile projectile[]) { // reads the file and allocates the projectiles arrays
+                                                                  // filled with the text file
+
+        try {
+            NumberFormat decimal = NumberFormat.getNumberInstance();
+            String prev;
+            int j = 0;
+            File myObj = new File("text.txt");
             Scanner myReader = new Scanner(myObj);
             Scanner scan = new Scanner(System.in);
-            String data = myReader.nextLine();
             do { // a do while loop to check if the user wants to check their previous games
                 System.out.println("Would you like to see previous games (Y/N)");
                 prev = scan.next().toLowerCase();
             } while (!prev.equals("y") && !prev.equals("n")); // the condition to check if they answered yes or no
-                while (myReader.hasNextLine()) { // checks if theres a next line in the file
-                    int a = 0;
-                    double b = 0;
-                    String c = "";
-            
-                    for (int j = 0; j <= count + 1; j++) { // goes through all the file lines
-                        if (j == 0) { // if j is 0 skip the line basically because its the title
-                            if (myReader.hasNextLine()) {
-                                data = myReader.nextLine();
-                            }
-                            
-                        } else {//extract all the data and puts them in corresponding variables 
-                            data = data.substring(data.indexOf('.') + 1);
-                            String name = (data.substring(0, data.indexOf(','))); //saves the name of the projectile into name variable
-                            data = data.substring(data.indexOf(',') + 1);
-            
-                            double dummyDistance = Double.parseDouble(data.substring(0, data.indexOf(','))); // saves the dummy distance into a double
-                            data = data.substring(data.indexOf(',') + 1);
-            
-                            double angle = Double.parseDouble(data.substring(0, data.indexOf(','))); // saves the angle into a double
-                            data = data.substring(data.indexOf(',') + 1);
-            
-                            double velocity = Double.parseDouble(data.substring(0, data.indexOf(','))); // save the velocity into a double
-                            data = data.substring(data.indexOf(',') + 1);
-            
-                            double grav = Double.parseDouble(data.substring(0, data.indexOf(','))); // save the gravity into a double 
-                            data = data.substring(data.indexOf(',') + 1);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (j == 0) {
+                    if (myReader.hasNextLine()) {
+                        System.out.println("");
+                        data = myReader.nextLine();
+                    }
+                }
+                
+                j++;
+                data = data.substring(data.indexOf('.') + 1);
+                String name = data.substring(0, data.indexOf(',')); // saves the name of the projectile into name
+                                                                    // variable
+                data = data.substring(data.indexOf(',') + 1);
+                double dummyDistance = Double.parseDouble(data.substring(0, data.indexOf(','))); // saves the dummy
+                                                                                                 // distance into a
+                                                                                                 // double
+                data = data.substring(data.indexOf(',') + 1);
 
-                            double height = Double.parseDouble(data); //save the height into a double
-            
-                            projectile[j-1] = new Projectile(name, height, velocity, angle, dummyDistance); //stores the value into a new projectile class
-                            if (height>0){
-                                projectile[j-1].setDistanceXkick(); //does the x kick math
-                                projectile[j-1].setDistanceYkick(); //does the y kick math
-                                projectile[j-1].setTimekick();
-                                projectile[j-1].setDistanceFromDummyKick();
-                            }else{
-                                projectile[j-1].setTimeThrow();
-                                projectile[j-1].setDistanceXthrow();
-                                projectile[j-1].setDistanceYthrow();
-                                projectile[j-1].setDistanceFromDummyThrow();
-                            }
-                            projectile[j-1].setAcceleration(grav);
-            
-                            if (myReader.hasNextLine()) {
-                                data = myReader.nextLine();
-                            }
-                        }
-                    }
+                double angle = Double.parseDouble(data.substring(0, data.indexOf(','))); // saves the angle into a
+                                                                                         // double
+                data = data.substring(data.indexOf(',') + 1);
+
+                double velocity = Double.parseDouble(data.substring(0, data.indexOf(','))); // save the velocity into a
+                                                                                            // double
+                data = data.substring(data.indexOf(',') + 1);
+
+                double grav = Double.parseDouble(data.substring(0, data.indexOf(','))); // save the gravity into a
+                                                                                        // double
+                data = data.substring(data.indexOf(',') + 1);
+
+                double height = Double.parseDouble(data); // save the height into a double
+
+
+                projectile[j - 1] = new Projectile(name, height, velocity, angle, dummyDistance); // stores the value
+                                                                                                  // into a new
+                                                                                                  // projectile class
+                projectile[j - 1].setDistanceXkick(); // does the x kick math
+                projectile[j - 1].setDistanceYkick(); // does the y kick math
+                projectile[j - 1].setTimekick();
+                projectile[j - 1].setDistanceFromDummyKick();
+                projectile[j - 1].setAcceleration(grav);
+
+            }
+            if (prev.equals("y")) { // checks if the user wants to check their previous attempts
+                System.out.println("Names, Distance from dummy, Angle, Velocity, Gravity, Height");
+                for (int i = 0; i <= count-1; i++) {
+                    System.out.println(i + 1 + "." + projectile[i].getNames() + "," + projectile[i].getDummy() + ","// displays the previous attempts to the user if they want
+                            + decimal.format(projectile[i].getAngle() * 180 / Math.PI) + ","
+                            + projectile[i].getVelocity() + "," + projectile[i].getAcceleration() + ","
+                            + projectile[i].getHeight() + "\n");
                 }
-                if (prev.equals("y")) {
-                    System.out.println("Names, Distance from dummy, Angle, Velocity, Gravity, Height");
-                    for (int i = 0; i<count+1; i++){
-                        System.out.println(i + 1 + "." + projectile[i].getNames() + "," + projectile[i].getDummy() + "," + decimal.format(projectile[i].getAngle()*180/Math.PI) + "," + projectile[i].getVelocity() + "," + projectile[i].getAcceleration() +","+ projectile[i].getHeight()+ "\n");
-                    }
-                }
-                myReader.close(); 
-            
-        } catch (FileNotFoundException e) {
+            }
+            myReader.close(); // close reader
+
+        } catch (FileNotFoundException e) { // basic catch exceptions
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
@@ -216,6 +224,7 @@ public class Main {
     public static Projectile[] sort(Projectile[] players) { // bubble sort the users in order to determine whos first,
                                                             // second, third and so on
         int n = players.length;
+        System.out.println(n);
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
 
@@ -406,7 +415,7 @@ public class Main {
             System.out.println("How many people are playing: "); // ask the user for how many people are playing
             int numPeople = myScanner.nextInt();
             int count = count();
-            players = new Projectile[numPeople + count+1];
+            players = new Projectile[numPeople + count];
             
             read(count,players);
 
@@ -420,23 +429,19 @@ public class Main {
                 distances = new double[numPeople];
                 double dummyDistance = dummy(myScanner);
 
-                for (int i = count; i < numPeople+count; i++) {
+                for (int i = count; i <= numPeople+count-1; i++) {
                     // Store info of players and ask for name, angle, and velocity
                     String name = name(myScanner);
                     double angle = angle(myScanner);
                     double velocity = velocity(myScanner);
 
-                    players[i+1] = new Projectile(name, velocity, angle, dummyDistance);
-                    players[i+1].setDistanceXkick(); // davit check over to see if we can make this better
-                    players[i+1].setDistanceYkick(); // it works but might lose us marks
-                    players[i+1].setTimekick();
-                    players[i+1].setDistanceFromDummyKick();
-                    players[i+1].setAcceleration(grav);
+                    players[i] = new Projectile(name, velocity, angle, dummyDistance);
+                    players[i].setDistanceXkick(); // davit check over to see if we can make this better
+                    players[i].setDistanceYkick(); // it works but might lose us marks
+                    players[i].setTimekick();
+                    players[i].setDistanceFromDummyKick();
+                    players[i].setAcceleration(grav);
 
-                }
-                // store the users info based on how close they got to the dummy
-                for (int i = 0; i < numPeople; i++) {
-                    distances[i] = players[i].getDistanceFromDummy();
                 }
 
                 // sort the distances array here, from least to greatest based on how close they
@@ -449,7 +454,7 @@ public class Main {
                     System.out.println(message);
                     // Display if more than 1 player participating
                 } else {
-                    for (int i = 0; i < numPeople+count+1; i++) { // Display users in order from closest to furthest and
+                    for (int i = 0; i < numPeople+count; i++) { // Display users in order from closest to furthest and
                                                                // their distance from the dummy
                         Projectile element = players[i];
                         System.out.println(i + 1 + ". " + element.getNames() + "- Landed "
@@ -459,7 +464,7 @@ public class Main {
             } else {
                 distances = new double[numPeople];
                 double dummyDistance = dummy(myScanner);
-                for (int i = count; i < numPeople+count; i++) {
+                for (int i = count; i <= numPeople+count-1; i++) {
                     // Store info of players
                     String name = name(myScanner);
                     double angle = angle(myScanner);
@@ -467,16 +472,17 @@ public class Main {
                     double height = height(myScanner);
                     // call the class and make a player array
 
-                    players[i+1] = new Projectile(name, height, velocity, angle, dummyDistance);
-                    players[i+1].setTimeThrow();
-                    players[i+1].setDistanceXthrow();
-                    players[i+1].setDistanceYthrow();
-                    players[i+1].setDistanceFromDummyThrow();
+                    players[i] = new Projectile(name, height, velocity, angle, dummyDistance);
+                    players[i].setTimeThrow();
+                    players[i].setDistanceXthrow();
+                    players[i].setDistanceYthrow();
+                    players[i].setDistanceFromDummyThrow();
 
                 }
                 // store the users info based on how close they got to the dummy
                 for (int i = 0; i < numPeople; i++) {
                     distances[i] = players[i].getDistanceFromDummy();
+                    System.out.println(distances[i]);
                 }
 
                 players = sort(players);
@@ -487,7 +493,7 @@ public class Main {
 
                     // Display if more than 1 player participating
                 } else {
-                    for (int i = 0; i < numPeople+count+1; i++) {
+                    for (int i = 0; i < numPeople+count; i++) {
                         Projectile element = players[i];// Display users in order from closest to furthest and their
                                                         // distance from the dummy
                         System.out.println(i + 1 + ". " + element.getNames() + "- Landed "
